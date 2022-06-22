eed4393a7132102bb63d97cfb8a112d6c3bffeae
DocRagnarok <106695471+DocRagnarok@users.noreply.github.com>
2022-06-22 08:56:31 +0700

    music add

--- src/app/qapp/qapp.component.ts
+++ src/app/qapp/qapp.component.ts
@@ -11,12 +11,28 @@ export class QappComponent implements OnInit {
   text: string = '';
   press1: boolean = false;
   text2: string = '';
+  music = new Audio();
+
+  closemusic() {
+    this.music.pause();
+  }
   buttonclick() {
-    document.getElementById('interface').style.display = 'none';
-    alert('สวัสดี  ' + this.text + '   เรามาเริ่มทำควิซกันเลย');
-    document.getElementById('quiz1').style.display = 'flex';
+    if (this.text == '') {
+      alert('ใส่ชื่อก่อนเล่นสิวะ');
+    } else {
+      alert('สวัสดี  ' + this.text + '   เรามาเริ่มทำควิซกันเลย');
+      document.getElementById('interface').style.display = 'none';
+      document.getElementById('quiz1').style.display = 'flex';
+      this.music.src = '../assets/payon.mp3';
+      this.music.load();
+      this.music.play();
+    }
   }
   ansbutton1() {
+    if (this.text2 == '') {
+      alert('การไม่ตอบก็คือคำตอบ - sun tzu ///ถุย ใส่คำตอบด้วยสิวะ');
+      return;
+    }
     if (this.text2 == '2') {
       alert('ยินดีด้วย  ' + this.text + '  คุณตอบถูก ! ไปข้อต่อไปกันเลย !!');
     } else {
--- src/app/qapp/qapp.module.ts
+++ src/app/qapp/qapp.module.ts
@@ -4,9 +4,19 @@ import { QappComponent } from './qapp.component';
 import { FormsModule } from '@angular/forms';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { AccordionModule } from 'primeng/accordion';
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
   declarations: [QappComponent],
-  imports: [CommonModule, FormsModule, MatSliderModule, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatSliderModule,
+    MatPaginatorModule,
+    AccordionModule,
+    ButtonModule,
+  ],
 
   exports: [QappComponent],
 })
