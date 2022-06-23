108c39fd50e20b43f3ab43591d17e2b886d262c0
Your Name <you@example.com>
2022-06-23 21:41:21 +0700

    updaterouting

---
+++ src/app/qapp-q1/qapp-q1.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, 
+    FormsModule,
+    MatSliderModule,
+    MatPaginatorModule,
+    AccordionModule,
+    ButtonModule,
+  ]
+})
+export class QappQ1Module { }
