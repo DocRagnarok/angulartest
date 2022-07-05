acb3392be1884ae78ed3dcd5538148e0c058c5a1
DocRagnarok <106695471+DocRagnarok@users.noreply.github.com>
2022-07-05 12:43:12 +0700

    bgupdate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MatSliderModule } from '@angular/material/slider';
 import { QappQ2Component } from './qapp-q2/qapp-q2.component';
 import { HttpClientModule } from '@angular/common/http';
 import { QappQ1Component } from './qapp-q1/qapp-q1.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, QappQ2Component, QappQ1Component],
@@ -22,6 +25,9 @@ import { QappQ1Component } from './qapp-q1/qapp-q1.component';
     QappModule,
     MatSliderModule,
     HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/nav-bar/nav-bar.module.ts
+++ src/nav-bar/nav-bar.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar.component';
 import { AppRoutingModule } from 'src/app/app.route';
 import { PianoappModule } from 'src/app/pianoapp/pianoapp.module';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [NavBarComponent],
-  imports: [CommonModule, AppRoutingModule, PianoappModule],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    PianoappModule,
+    MatMenuModule,
+    MatToolbarModule,
+  ],
+  providers: [],
   exports: [NavBarComponent],
 })
 export class NavBarModule {}
